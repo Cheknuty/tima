03b974ca9ecbf55383b36a200b1061a7f2e8a492
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-29 11:13:41 +0500

    v

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+var temp = "";
+
 function givePrompt() {
     var input = document.getElementById('ClientEmail');
     var value = input.value;
-    const arr = value.split("").filter(letter => letter === "@")
-    if (value.includes("@") && arr.length === 1) {
+    const str = input.value.split('@');
+    if (value.includes("@") && str.length == 2) {
         alert(value);
-        var currentEmail = input.value.split('@').pop();
-        input.value = null
-        
+        var currentEmail = str.pop();
         if (currentEmail.length == 0) {
-            const str = input.value.split('@');
-            input.value = null
-            input.value =  `${str.join("")}@gmail.com`;
-            // console.log(`${str.join("")}@gmail.com`);
+            input.value += "gmail.com";
+            temp = input.value;
             // input.focus();
             // input.setSelectionRange(value.length, input.value.length);
             alert("+_+_+_+_" + input.value);
         }
+    } else if(str.length > 2){
+        input.value = temp;
     }
 }
