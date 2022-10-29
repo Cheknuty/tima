8cbb483734ca8ef4736ee2df7908a5db12d0dded
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-29 10:37:57 +0500

    finish

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 function givePrompt() {
     var input = document.getElementById('ClientEmail');
     var value = input.value;
-    if (value.includes("@")) {
-    //     alert(value);
-    //     var currentEmail = input.value.split('@').pop();
+    const arr = value.split("").filter(letter => letter === "@")
+    if (value.includes("@") && arr.length === 1) {
+        alert(value);
+        var currentEmail = input.value.split('@').pop();
 
-    //     if (currentEmail.length == 0) {
+        if (currentEmail.length == 0) {
             input.value += "gmail.com";
-            // input.focus();
-            // input.setSelectionRange(value.length, input.value.length);
+            input.focus();
+            input.setSelectionRange(value.length, input.value.length);
             alert("+_+_+_+_" + input.value);
-    //     }
+        }
     }
 }
