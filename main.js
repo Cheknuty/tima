6d0525ba5c658735edcf2e24cc184f9d23737cc8
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-29 10:45:49 +0500

    ficx

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ function givePrompt() {
     var input = document.getElementById('ClientEmail');
     var value = input.value;
     const arr = value.split("").filter(letter => letter === "@")
+    const str = input.value.split('@');
+    console.log(str);
     if (value.includes("@") && arr.length === 1) {
         alert(value);
         var currentEmail = input.value.split('@').pop();
 
         if (currentEmail.length == 0) {
-            input.value += "gmail.com";
+            input.value =  `${str.join("")}@gmail.com`;
             // input.focus();
             // input.setSelectionRange(value.length, input.value.length);
             alert("+_+_+_+_" + input.value);
