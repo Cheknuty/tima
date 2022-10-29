897f4f7d63c9407580e1a303350fa69e52de516b
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-29 10:21:06 +0500

    test

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ function givePrompt() {
     var input = document.getElementById('ClientEmail');
     var value = input.value;
     if (value.includes("@")) {
+        alert(value);
         var currentEmail = input.value.split('@').pop();
 
         if (currentEmail.length == 0) {
             input.value += "gmail.com";
             input.focus();
             input.setSelectionRange(value.length, input.value.length);
+            alert("+_+_+_+_" + input.value);
         }
     }
 }
