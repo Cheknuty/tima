5fe9381ff17950616741ec41d60115a5d1eac7fc
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-29 12:28:54 +0500

    f

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 var temp = "";
+var isUserEdit = false;
 
 function givePrompt() {
     var input = document.getElementById('ClientEmail');
     var value = input.value;
     const str = input.value.split('@');
     if (value.includes("@") && str.length == 2) {
-        alert(value);
         var currentEmail = str.pop();
         if (currentEmail.length == 0) {
             input.value += "gmail.com";
             temp = input.value;
-            // input.focus();
-            // input.setSelectionRange(value.length, input.value.length);
-            alert("+_+_+_+_" + input.value);
+            if(isUserEdit){
+                isUserEdit = false;
+            }
+            setTimeout(() => {
+                isUserEdit = true;
+            }, 1000);
+            input.focus();
+            input.setSelectionRange(value.length, input.value.length);
         }
-    } else if(str.length > 2){
+    } else if(str.length > 2 && !isUserEdit){
         input.value = temp;
+        isUserEdit = true;
     }
 }
