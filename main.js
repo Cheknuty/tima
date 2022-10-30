7ed554aff45423ff050315037bd54153cd3e1fbe
azizovxikmatjon04 <azizovxikmatjon04@gmail.com>
2022-10-30 13:03:51 +0500

    dw

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-var temp = "";
-var isUserEdit = false;
-
-function givePrompt() {
-    var input = document.getElementById('ClientEmail');
-    var value = input.value;
-    const str = input.value.split('@');
-    if (value.includes("@") && str.length == 2) {
-        var currentEmail = str.pop();
-        if (currentEmail.length == 0) {
-            input.value += "gmail.com";
-            temp = input.value;
-            if(isUserEdit){
-                isUserEdit = false;
-            }
-            setTimeout(() => {
-                isUserEdit = true;
-            }, 1000);
-            input.focus();
-            input.setSelectionRange(value.length, input.value.length);
-        }
-    } else if(str.length > 2 && !isUserEdit){
-        input.value = temp;
-        isUserEdit = true;
-    }
+document.getElementById("ClientEmail").onkeydown = function(evt) {
+    console.log(evt);
 }
